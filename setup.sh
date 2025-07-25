fd1bc31c96491705cbbace1d68b528f3972cf3bd
CodeNinja9900 <167338735+CodeNinja9900@users.noreply.github.com>
2025-07-26 01:49:17 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+2. Install Git LFS and fetch the actual .pkl files
+apt-get update && apt-get install -y git-lfs
+git lfs install
+git lfs pull
